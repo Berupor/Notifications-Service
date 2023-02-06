fba6b6303a9216c1d1416e33f4a0366e2f9e73d1
Evgeniy Zelenov <zenek.zeka13@gmail.com>
2023-02-06 17:22:13 +0600

    Update main file

--- notifications_service/src/main.py
+++ notifications_service/src/main.py
@@ -2,7 +2,9 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 
+from api.v1 import events
 from core.config import settings
+from message_broker.rabbitmq.rabbitmq_broker import get_rabbitmq
 
 app = FastAPI(
     title="API для получения и обработки нотификаций пользователя",
@@ -11,6 +13,21 @@ app = FastAPI(
     openapi_url="/ugc/api/openapi.json",
     default_response_class=ORJSONResponse,
 )
+
+
+@app.on_event("startup")
+async def startup():
+    rabbitmq = get_rabbitmq()
+    await rabbitmq.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    rabbitmq = get_rabbitmq()
+    await rabbitmq.close()
+
+
+app.include_router(events.router, prefix="/api/v1/notification", tags=["Notifications"])
 
 if __name__ == "__main__":
     uvicorn.run(
