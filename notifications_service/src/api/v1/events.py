139750e78dfbbd79e7dc3ff62a47c209dd703267
Evgeniy Zelenov <zenek.zeka13@gmail.com>
2023-02-06 20:15:12 +0600

    Finish api

--- notifications_service/src/api/v1/events.py
+++ notifications_service/src/api/v1/events.py
@@ -1,37 +1,50 @@
 from http import HTTPStatus
 
-from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi import APIRouter, Depends
 
-from services.user_service import get_user_service, UserService
-from message_broker.rabbitmq.rabbitmq_broker import get_rabbitmq, RabbitMQBroker
-
+from message_broker.rabbitmq.rabbitmq_broker import (RabbitMQBroker,
+                                                     get_rabbitmq)
 from models.event import RequestEventModel, ResponseEventModel
+from services.template_service import TemplateService, get_template_service
+from services.user_service import UserService, get_user_service
 
 router = APIRouter()
 
+queue_priority = {1: "low", 2: "medium", 3: "high"}
+
 
 @router.post(
-    "email/{user_id}",
+    "/email/{user_id}",
     summary="Формирование данных для почтового уведомления.",
     description="Получение шаблона, почты пользователя и приведение данных к общему формату.",
     response_description="Статус обработки данных.",
-    response_model=ResponseEventModel,
 )
 async def email_notification(
         event: RequestEventModel,
         user_id: str,
-        request: Request,
         user_service: UserService = Depends(get_user_service),
-        message_service: RabbitMQBroker = Depends(get_rabbitmq)
-):
+        template_service: TemplateService = Depends(get_template_service),
+        message_service: RabbitMQBroker = Depends(get_rabbitmq),
+) -> int:
     """Processing received event data.
     Args:
-        event: event data.
-        request: request value.
+        event: Event data
         user_id: Id user
+        user_service: Service for working with user
+        template_service: Service for working with templates
+        message_service: Service for working with queue
     Returns:
         Execution status.
     """
     user = await user_service.find_one(id=user_id)
+    template = await template_service.find_one(event_name=event.name)
 
-    return HTTPStatus.CREATED
+    ready_data = ResponseEventModel(
+        template=template.html,
+        user=user.to_dict(), type="email"
+    )
+
+    await message_service.produce(
+        ready_data.json(), queue_name=queue_priority[event.priority]
+    )
+    return HTTPStatus.ACCEPTED
--- notifications_service/src/db/fake_data.py
+++ notifications_service/src/db/fake_data.py
@@ -2,6 +2,8 @@ import psycopg2
 import random
 import string
 from core.config import settings
+import uuid
+
 
 def random_string(stringLength=10):
     letters = string.ascii_lowercase
@@ -16,17 +18,17 @@ def random_number(n=10):
 
 conn = psycopg2.connect(
     host="localhost",
-    database=,
-    user="postgres",
-    password="password"
+    database=settings.postgres.dbname,
+    user=settings.postgres.user,
+    password=settings.postgres.password
 )
 
 cur = conn.cursor()
 
 # create table if not exists
 cur.execute('''
-    CREATE TABLE IF NOT EXISTS user (
-        id SERIAL PRIMARY KEY,
+    CREATE TABLE IF NOT EXISTS "user" (
+        id UUID PRIMARY KEY,
         email VARCHAR(255) NOT NULL,
         name VARCHAR(255) NOT NULL,
         phone VARCHAR(255) NOT NULL
@@ -35,13 +37,13 @@ cur.execute('''
 
 # insert fake data
 fake_data = [
-    (random_string() + "@email.com", random_string(), random_number())
+    (str(uuid.uuid1()), random_string() + "@email.com", random_string(), random_number())
     for i in range(10)
 ]
 
 cur.executemany('''
-    INSERT INTO user (email, name, phone)
-    VALUES (%s, %s, %s);
+INSERT INTO "user" (id, email, name, phone)
+VALUES (%s, %s, %s, %s);
 ''', fake_data)
 
 conn.commit()
