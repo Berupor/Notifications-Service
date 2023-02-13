c6bfae51233e45a15d20854faceec8c9e44c16be
Evgeniy Zelenov <zenek.zeka13@gmail.com>
2023-02-13 19:28:45 +0600

    Test ci

--- .github/tools/send_message.py
+++ .github/tools/send_message.py
@@ -8,8 +8,10 @@ async def send_telegram_message(api_token, chat_id, message):
     await bot.send_message(chat_id=chat_id, text=message)
 
 
-api_token = os.environ["API_TOKEN"]
+api_token = os.environ["TELEGRAM_API_TOKEN"]
 chat_id = os.environ["CHAT_ID"]
-message = "CI tests!"
+ci_message = os.environ["MESSAGE"]
+
+message = "CI tests " + ci_message
 
 asyncio.run(send_telegram_message(api_token, chat_id, message))
