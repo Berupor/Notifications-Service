4be745e7e8e1afff7057e6a66bea11dc8be9a45c
Evgeniy Zelenov <zenek.zeka13@gmail.com>
2023-02-13 19:33:50 +0600

    Test ci

--- .github/tools/send_message.py
+++ .github/tools/send_message.py
@@ -10,8 +10,16 @@ async def send_telegram_message(api_token, chat_id, message):
 
 api_token = os.environ["TELEGRAM_API_TOKEN"]
 chat_id = os.environ["CHAT_ID"]
-ci_message = os.environ["MESSAGE"]
 
-message = "CI tests " + ci_message
+if "CI_SUCCESS" in os.environ:
+    commit_hash = os.environ["CI_COMMIT_HASH"]
+    project_name = os.environ["CI_PROJECT_NAME"]
+    step_name = os.environ.get("CI_STEP_NAME")
+    result = "successfully" if os.environ["CI_SUCCESS"] == "true" else "with error"
+    message = f"{project_name}: Pipeline for {commit_hash} finished {result}!"
+    if step_name:
+        message += f"\n{step_name} failed"
+else:
+    message = "CI tests"
 
 asyncio.run(send_telegram_message(api_token, chat_id, message))
