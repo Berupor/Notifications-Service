0d09e0856f6f8f01b32f9cb67eb4e0ca9b2f4f0a
Evgeniy Zelenov <zenek.zeka13@gmail.com>
2023-02-14 15:00:05 +0600

    Update action

--- .github/tools/send_message.py
+++ .github/tools/send_message.py
@@ -8,18 +8,8 @@ async def send_telegram_message(api_token, chat_id, message):
     await bot.send_message(chat_id=chat_id, text=message)
 
 
-api_token = os.environ["TELEGRAM_API_TOKEN"]
+api_token = os.environ["API_TOKEN"]
 chat_id = os.environ["CHAT_ID"]
-
-if "CI_SUCCESS" in os.environ:
-    commit_hash = os.environ["CI_COMMIT_HASH"]
-    project_name = os.environ["CI_PROJECT_NAME"]
-    step_name = os.environ.get("CI_STEP_NAME")
-    result = "successfully" if os.environ["CI_SUCCESS"] == "true" else "with error"
-    message = f"{project_name}: Pipeline for {commit_hash} finished {result}!"
-    if step_name:
-        message += f"\n{step_name} failed"
-else:
-    message = "CI tests"
+message = os.environ["MESSAGE"]
 
 asyncio.run(send_telegram_message(api_token, chat_id, message))
