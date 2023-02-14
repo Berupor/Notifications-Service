f19fe4a864b54b42229595b9708f44ee18581419
nikita <nikitakhb@gmail.com>
2023-02-15 00:40:06 +1000

    handler, updated architecture

--- notifications_handler/models/notification.py
+++ notifications_handler/models/notification.py
@@ -11,9 +11,9 @@ class MessageHandler(BaseModel):
     create: datetime
 
 
-class NotificationHandler(BaseModel):
+class Notification(BaseModel):
     id_user: Optional[str]
     created_at: Optional[str]
     notification_name: Optional[str]
     priority: Optional[int] = Field(default=0, ge=0, le=3)
-    data: MessageHandler
+    data: Optional[str]
