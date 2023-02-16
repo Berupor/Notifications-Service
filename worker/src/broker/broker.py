7c6bbbd588e7468e496c715218b03b4552e0376a
Not-Elon-But-Maks <113140181+Not-Elon-But-Maks@users.noreply.github.com>
2023-02-16 22:27:37 +0300

    добавил анатацию типов

--- worker/src/broker/broker.py
+++ worker/src/broker/broker.py
@@ -1,18 +1,18 @@
 from json import loads
 import backoff
 
-import aiormq
+from aiormq import connect as mq_connect, Connection, Channel, abc
 
 
 class RabbitMq:
     def __init__(self, host: str, port: int, username: str, password: str):
-        self.host = host
-        self.port = port
-        self.username = username
-        self.password = password
-        self.connection = None
-        self.channel = None
-        self.message = None
+        self.host: str = host
+        self.port: int = port
+        self.username: str = username
+        self.password: str = password
+        self.connection: Connection
+        self.channel: Channel
+        self.message: abc.DeliveredMessage
 
     @backoff.on_exception(
         backoff.expo,
@@ -23,7 +23,7 @@ class RabbitMq:
         max_tries=10,
     )
     async def connect(self):
-        self.connection = await aiormq.connect(
+        self.connection = await mq_connect(
             f"amqp://{self.username}:{self.password}@{self.host}//"
         )
         self.channel = await self.connection.channel()
@@ -32,7 +32,7 @@ class RabbitMq:
         if self.connection:
             await self.connection.close()
 
-    async def save_message(self, message: aiormq.abc.DeliveredMessage):
+    async def save_message(self, message: abc.DeliveredMessage):
         self.message = loads(message.body.decode("utf-8"))
         return self
 
